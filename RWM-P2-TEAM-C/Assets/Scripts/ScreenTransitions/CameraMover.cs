02808d5e03678c828e1a074d62ae343d5f36c54e
Joshua Dunne <c00241588@itcarlow.ie>
2021-11-22 12:24:57 +0000

    Pushing changes so I can move to a different branch

---
+++ RWM-P2-TEAM-C/Assets/Scripts/ScreenTransitions/CameraMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public Vector2 transitionPoint;
+
+    void StartMovement()
+    {
+        transitionPoint = this.gameObject.GetComponent<ScreenTransition>().transitionPoint;
+    }
+
+    IEnumerator movement()
+    {
+
+    }
+}
