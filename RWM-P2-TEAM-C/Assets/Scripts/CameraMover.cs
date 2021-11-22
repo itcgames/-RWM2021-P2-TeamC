7823392f5f9595e37b9cc385b238dc6275211fc2
Joshua Dunne <c00241588@itcarlow.ie>
2021-11-22 16:09:31 +0000

    ZS-33 Moving the Camera

--- RWM-P2-TEAM-C/Assets/Scripts/CameraMover.cs
+++ RWM-P2-TEAM-C/Assets/Scripts/CameraMover.cs
@@ -6,24 +6,60 @@ public class CameraMover : MonoBehaviour
 {
     public Camera mainCam;
     public Vector2 transitionPoint;
+    public float speed = 0.1f;
 
-    void StartMovement()
+    public void StartMovement()
     {
-        transitionPoint = GetComponent<ScreenTransition>().transitionPoint;
-        StartCoroutine(movement());
-
         mainCam = Camera.main;
+        transitionPoint = GetComponent<ScreenTransition>().transitionPoint;
+        StartCoroutine(movement()); 
     }
 
     IEnumerator movement()
     {
-        while(mainCam.transform.position.x != transitionPoint.x
-            && mainCam.transform.position.y != transitionPoint.y)
+        if(GetComponent<ScreenTransition>().type == ScreenTransition.transitionTypes.HORIZONTAL)
         {
-            Debug.Log("moving camera");
+            if (mainCam.transform.position.x < transitionPoint.x)
+            {
+                while(mainCam.transform.position.x <= transitionPoint.x)
+                {
+                    mainCam.transform.position = new Vector3(mainCam.transform.position.x + speed, mainCam.transform.position.y, mainCam.transform.position.z);
 
-            yield return new WaitForSeconds(0.1f);
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+            else if (mainCam.transform.position.x > transitionPoint.x)
+            {
+                while (mainCam.transform.position.x >= transitionPoint.x)
+                {
+                    mainCam.transform.position = new Vector3(mainCam.transform.position.x - speed, mainCam.transform.position.y, mainCam.transform.position.z);
+
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
         }
+        if (GetComponent<ScreenTransition>().type == ScreenTransition.transitionTypes.HORIZONTAL)
+        {
+            if (mainCam.transform.position.y < transitionPoint.y)
+            {
+                while (mainCam.transform.position.y <= transitionPoint.y)
+                {
+                    mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y + speed, mainCam.transform.position.z);
+
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+            else if (mainCam.transform.position.y > transitionPoint.y)
+            {
+                while (mainCam.transform.position.y >= transitionPoint.y)
+                {
+                    mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y - speed, mainCam.transform.position.z);
+
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+        }
+
 
         Debug.Log("camera reached end");
         yield break;
