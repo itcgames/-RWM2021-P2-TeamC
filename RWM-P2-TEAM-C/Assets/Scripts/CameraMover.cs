d3f8342d44d845754419ec5c11a051258d1f4bac
Joshua Dunne <c00241588@itcarlow.ie>
2021-11-22 15:15:17 +0000

    When a Screen Transition begins, the Camera will start moving. When the Camera reaches the end Transition Point, it will stop moving.

---
+++ RWM-P2-TEAM-C/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public Camera mainCam;
+    public Vector2 transitionPoint;
+
+    void StartMovement()
+    {
+        transitionPoint = GetComponent<ScreenTransition>().transitionPoint;
+        StartCoroutine(movement());
+
+        mainCam = Camera.main;
+    }
+
+    IEnumerator movement()
+    {
+        while(mainCam.transform.position.x != transitionPoint.x
+            && mainCam.transform.position.y != transitionPoint.y)
+        {
+            Debug.Log("moving camera");
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        Debug.Log("camera reached end");
+        yield break;
+    }
+}
