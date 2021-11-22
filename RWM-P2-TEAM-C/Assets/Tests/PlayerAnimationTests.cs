37f51db966890756bddbfb3cad02d43bffafb4b3
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-11-22 17:10:07 +0000

    Added in left and right animations

---
+++ RWM-P2-TEAM-C/Assets/Tests/PlayerAnimationTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class PlayerAnimationTests
+    {
+        private GameObject _player;
+        private Animator _animator;
+        private Runtime2DMovement _2dMovement;
+        private PlayerController _playerController;
+        [SetUp]
+        public void Setup()
+        {
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            SceneManager.UnloadSceneAsync("Game");
+        }
+
+        [UnityTest]
+        public IEnumerator LeftAnimationTest()
+        {
+            setUpPlayer();
+            _2dMovement.moveLeft();
+            _playerController.handleLeftAnimation();
+            yield return new WaitForSeconds(2.0f);
+            Assert.AreEqual(true, _animator.GetBool("movingLeft"));
+        }
+
+
+        private void setUpPlayer()
+        {
+            _player = GameObject.Find("Player");
+            _animator = _player.GetComponent<Animator>();
+            _2dMovement = _player.GetComponent<Runtime2DMovement>();
+            _playerController = _player.GetComponent<PlayerController>();
+        }
+    }
+}
