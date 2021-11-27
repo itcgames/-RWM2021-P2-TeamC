07323ff199a5cfd596f3aa7ce9bde5ba87a74a84
Richard Fleming <c00241212@itcarlow.ie>
2021-11-27 14:54:26 +0000

    Task movement (#6)
    * Added Basic Implementation
    
    * Added Basic Testing

---
+++ RWM-P2-TEAM-C/Assets/Scripts/Bomber.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomber : MonoBehaviour
+{
+    // Enemy's rigidbody
+    Rigidbody2D rb;
+
+    // Enemy Health
+    public float maxHealth = 5.0f;
+    // Enemy movement speed
+    public float speed = 5.0f;
+    // Player GameObject
+    public GameObject player;
+    // Current Health
+    private float health;
+
+    // Bombing Range
+    public float range = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        health = maxHealth;
+        // Movement
+        rb.velocity = new Vector2(-speed, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
