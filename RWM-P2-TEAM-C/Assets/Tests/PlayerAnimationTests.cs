9c5669e1e5ecf70e3e4b005f0f9d9b98ba821dde
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-11-28 19:06:50 +0000

    Animation Finished with all tests passing JIRA ID [ZS-35]  (#9)
    * Added idle animation for megaman
    
    * 2d left right animations (#7)
    
    * Added left animations
    
    * Added in left and right animations
    
    * Added first test for left movement
    
    * Added passing tests
    
    * Updated animations to make it feel more like megaman
    
    * 2d jumping animation (#8)
    
    * Added in jump animation
    
    * Added passing tests
    
    * Task Finished JIRA ID [ZS-37]

---
+++ RWM-P2-TEAM-C/Assets/Tests/PlayerAnimationTests.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class PlayerAnimationTests
+    {
+        private GameObject _player;
+        private Animator _animator;
+        private Runtime2DMovement _2dMovement;
+        private PlayerController _playerController;
+
+        [SetUp]
+        public void Setup()
+        {
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            SceneManager.UnloadSceneAsync("Game");
+        }
+
+        [UnityTest]
+        public IEnumerator LeftAnimationTest()
+        {
+            setUpPlayer();
+            _2dMovement.handleLeftInput();
+            _2dMovement.moveLeft();
+            _playerController.handleLeftAnimation();
+            yield return new WaitForSeconds(1.0f);
+            Assert.AreEqual(true, _animator.GetBool("movingLeft"));
+        }
+
+        [UnityTest]
+        public IEnumerator RightAnimationTest()
+        {
+            setUpPlayer();
+            _2dMovement.handleRightInput();
+            _2dMovement.moveRight();
+            _playerController.handleRightAnimation();
+            yield return new WaitForSeconds(1.0f);
+            Assert.AreEqual(true, _animator.GetBool("movingRight"));
+        }
+
+        [UnityTest]
+        public IEnumerator IdleAnimationTest()
+        {
+            setUpPlayer();
+            yield return new WaitForSeconds(1.0f);
+            Assert.AreEqual(true, _animator.GetBool("idle"));
+        }
+
+        [UnityTest]
+        public IEnumerator JumpAnimationTestWhileIdle()
+        {
+            setUpPlayer();
+            yield return new WaitForSeconds(1.0f);
+            _2dMovement.handleJumpInput();
+            _playerController.handleJumpAnimationWhileIdle();
+            yield return new WaitForSeconds(0.2f);
+            Assert.AreEqual(false, _animator.GetBool("grounded"));
+            yield return new WaitForSeconds(0.2f);
+            Assert.AreEqual(true, _animator.GetBool("grounded"));
+            Assert.AreEqual(true, _animator.GetBool("idle"));
+        }
+
+        [UnityTest]
+        public IEnumerator JumpAnimationTestWhileWalking()
+        {
+            setUpPlayer();
+            yield return new WaitForSeconds(1.0f);
+            _2dMovement.moveRight();
+            _playerController.handleRightAnimation();
+            Assert.AreEqual(true, _animator.GetBool("movingRight"));
+            yield return new WaitForSeconds(0.5f);
+            _2dMovement.handleJumpInput();
+            _playerController.handleJumpAnimationWhileIdle();
+            yield return new WaitForSeconds(0.2f);
+            Assert.AreEqual(false, _animator.GetBool("grounded"));
+            yield return new WaitForSeconds(0.2f);
+            Assert.AreEqual(true, _animator.GetBool("grounded"));
+            Assert.AreEqual(true, _animator.GetBool("idle"));
+        }
+
+        private void setUpPlayer()
+        {
+            _player = GameObject.Find("Player");
+            _animator = _player.GetComponent<Animator>();
+            _2dMovement = _player.GetComponent<Runtime2DMovement>();
+            _playerController = _player.GetComponent<PlayerController>();
+        }
+    }
+}
