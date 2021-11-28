42a3fff1d286de3b83a0d9df27baa1121c1603ae
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-11-28 14:46:43 +0000

    2d jumping animation (#8)
    * Added in jump animation
    
    * Added passing tests
    
    * Task Finished JIRA ID [ZS-37]

--- RWM-P2-TEAM-C/Assets/Scripts/PlayerController.cs
+++ RWM-P2-TEAM-C/Assets/Scripts/PlayerController.cs
@@ -9,9 +9,14 @@ public class PlayerController : MonoBehaviour
     private Runtime2DMovement _2dMovement;
     void Start()
     {
-        _animator =   this.GetComponent<Animator>();
+        setUpPlayer();
+    }
+
+    void setUpPlayer()
+    {
+        _animator = this.GetComponent<Animator>();
         _2dMovement = this.GetComponent<Runtime2DMovement>();
-        _rb =         this.GetComponent<Rigidbody2D>();
+        _rb = this.GetComponent<Rigidbody2D>();
         _rb = this.GetComponent<Rigidbody2D>();
         if (!_rb)
         {
@@ -26,52 +31,94 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_2dMovement.getIsMovingLeft() && !_animator.GetBool("movingLeft"))
+        updatePlayerAnimationStates();
+    }
+
+    void updatePlayerAnimationStates()
+    {
+        if (_2dMovement.getIsMovingLeft() && _2dMovement.getIsGrounded() && !_animator.GetBool("movingLeft"))
         {
             handleLeftAnimation();
         }
-        else if (_2dMovement.getIsMovingRight())
+        else if (_2dMovement.getIsMovingRight() && _2dMovement.getIsGrounded() && !_animator.GetBool("movingRight"))
         {
             handleRightAnimation();
         }
-        else if(_rb.velocity.SqrMagnitude() <= 0 && !_animator.GetBool("idle"))
+        else if (!_2dMovement.getIsGrounded() && _animator.GetBool("idle"))
+        {
+            handleJumpAnimationWhileIdle();
+        }
+        else if ((_2dMovement.getIsMovingRight() || _2dMovement.getIsMovingLeft()) && !_2dMovement.getIsGrounded() && _animator.GetBool("grounded"))
+        {
+            handleJunpAnimationWhileWalking();
+        }
+        else if ((_2dMovement.getIsMovingRight() || _2dMovement.getIsMovingLeft()) && !_animator.GetBool("grounded"))
+        {
+            handleDirectionWhileJumping();
+        }
+        else if ((_rb.velocity.SqrMagnitude() <= 0 && _2dMovement.getIsGrounded()) && !_animator.GetBool("idle"))
         {
             handleIdleAnimation();
         }
     }
+
     public void handleLeftAnimation()
     {
-        if (_2dMovement.getIsMovingLeft() && !_animator.GetBool("movingLeft"))
-        {
-            Vector3 temp = transform.localScale;
-            if (temp.x < 0) { temp.x *= -1; }
-            transform.localScale = temp;
-            _animator.SetBool("movingLeft", true);
-            _animator.SetBool("movingRight", false);
-            _animator.SetBool("idle", false);
-        }
+        Vector3 temp = transform.localScale;
+        if (temp.x < 0) { temp.x *= -1; }
+        transform.localScale = temp;
+        _animator.SetBool("movingLeft", true);
+        _animator.SetBool("movingRight", false);
+        _animator.SetBool("grounded", true);
+        _animator.SetBool("idle", false);
     }
 
     public void handleRightAnimation()
     {
-        if (_2dMovement.getIsMovingRight())
-        {
-            Vector3 temp = transform.localScale;
-            if (temp.x > 0) { temp.x *= -1; }
-            transform.localScale = temp;
-            _animator.SetBool("movingRight", true);
-            _animator.SetBool("movingLeft", false);
-            _animator.SetBool("idle", false);
-        }
+        Vector3 temp = transform.localScale;
+        if (temp.x > 0) { temp.x *= -1; }
+        transform.localScale = temp;
+        _animator.SetBool("movingRight", true);
+        _animator.SetBool("movingLeft", false);
+        _animator.SetBool("grounded", true);
+        _animator.SetBool("idle", false);
+    }
+
+    public void handleJumpAnimationWhileIdle()
+    {
+        _animator.SetBool("grounded", false);
+        _animator.SetBool("idle", false);
     }
 
     public void handleIdleAnimation()
     {
-        if (_rb.velocity.SqrMagnitude() <= 0 && !_animator.GetBool("idle"))
+        _animator.SetBool("movingRight", false);
+        _animator.SetBool("movingLeft", false);
+        _animator.SetBool("grounded", true);
+        _animator.SetBool("idle", true);
+    }
+
+    public void handleJunpAnimationWhileWalking()
+    {
+        _animator.SetBool("movingLeft", false);
+        _animator.SetBool("movingRight", false);
+        _animator.SetBool("grounded", false);
+        _animator.SetBool("idle", false);
+    }
+
+    public void handleDirectionWhileJumping()
+    {
+        if (_2dMovement.getIsMovingLeft())
         {
-            _animator.SetBool("movingRight", false);
-            _animator.SetBool("movingLeft", false);
-            _animator.SetBool("idle", true);
+            Vector3 temp = transform.localScale;
+            if (temp.x < 0) { temp.x *= -1; }
+            transform.localScale = temp;
+        }
+        else if (_2dMovement.getIsMovingRight())
+        {
+            Vector3 temp = transform.localScale;
+            if (temp.x > 0) { temp.x *= -1; }
+            transform.localScale = temp;
         }
     }
 }
