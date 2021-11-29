511bb49767d76f28c1f2814b107339c1e16c0694
Joshua Dunne <44901016+Joshua-Dunne@users.noreply.github.com>
2021-11-29 10:22:01 +0000

    ZS-31 Screen Transition Movement (#17)
    * Pushing changes so I can move to a different branch
    
    * When a Screen Transition begins, the Camera will start moving.
    When the Camera reaches the end Transition Point, it will stop moving.
    
    * ZS-33 Moving the Camera
    
    * Components other than Camera and Audio Listener are disabled during Transition on Main Camera
    
    * Added passing tests
    
    * ZS-31 Screen Transition Movement Complete

---
+++ RWM-P2-TEAM-C/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public Camera mainCam;
+    public Vector2 transitionPoint;
+    public float speed = 0.1f;
+    public bool m_moving;
+
+    private List<Behaviour> heldComponents;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    public void StartMovement()
+    {
+        
+        transitionPoint = GetComponent<ScreenTransition>().transitionPoint;
+
+        foreach (Behaviour behaviour in GetComponents<Behaviour>())
+        {
+            if(behaviour.enabled)
+            {
+                if(behaviour != mainCam)
+                {
+                    behaviour.enabled = false;
+                }
+            }
+        }
+
+        // re-enable these afterwards
+        mainCam.enabled = true;
+        GetComponent<AudioListener>().enabled = true;
+
+        StartCoroutine(movement()); 
+    }
+
+    IEnumerator movement()
+    {
+        bool wentThroughMove = false;
+        if(!m_moving)
+        { // only do this enumerator if it isn't already happening, to stop overlapping
+            m_moving = true;
+            wentThroughMove = true;
+            if (GetComponent<ScreenTransition>().type == ScreenTransition.transitionTypes.HORIZONTAL)
+            {
+                if (mainCam.transform.position.x < transitionPoint.x)
+                {
+                    while (mainCam.transform.position.x <= transitionPoint.x)
+                    {
+                        mainCam.transform.position = new Vector3(mainCam.transform.position.x + speed, mainCam.transform.position.y, mainCam.transform.position.z);
+
+                        yield return new WaitForSeconds(0.1f);
+                    }
+                }
+                else if (mainCam.transform.position.x > transitionPoint.x)
+                {
+                    while (mainCam.transform.position.x >= transitionPoint.x)
+                    {
+                        mainCam.transform.position = new Vector3(mainCam.transform.position.x - speed, mainCam.transform.position.y, mainCam.transform.position.z);
+
+                        yield return new WaitForSeconds(0.1f);
+                    }
+                }
+            }
+            if (GetComponent<ScreenTransition>().type == ScreenTransition.transitionTypes.HORIZONTAL)
+            {
+                if (mainCam.transform.position.y < transitionPoint.y)
+                {
+                    while (mainCam.transform.position.y <= transitionPoint.y)
+                    {
+                        mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y + speed, mainCam.transform.position.z);
+
+                        yield return new WaitForSeconds(0.1f);
+                    }
+                }
+                else if (mainCam.transform.position.y > transitionPoint.y)
+                {
+                    while (mainCam.transform.position.y >= transitionPoint.y)
+                    {
+                        mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y - speed, mainCam.transform.position.z);
+
+                        yield return new WaitForSeconds(0.1f);
+                    }
+                }
+            }
+            Debug.Log("camera reached end");
+        }
+
+        // now that the mover is done, re-enable any disabled components
+
+        foreach (Behaviour behaviour in GetComponents<Behaviour>())
+        {
+            if (behaviour.enabled)
+            {
+                if (behaviour != mainCam)
+                {
+                    behaviour.enabled = true;
+                }
+            }
+        }
+
+        yield break;
+    }
+}
---
+++ RWM-P2-TEAM-C/Assets/Scripts/TempInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // when the GameObjects collider arrange for this GameObject to travel to the left of the screen
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Camera.main.GetComponent<CameraMover>().StartMovement();
+        }
+    }
+}
