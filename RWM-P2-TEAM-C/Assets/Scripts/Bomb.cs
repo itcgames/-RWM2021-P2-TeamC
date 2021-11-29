c613c69a8ce863712f6d0b8bc360f994b8daf282
Richard Fleming <richard.fleming164@gmail.com>
2021-11-29 04:02:36 +0000

    Added basic implementation

---
+++ RWM-P2-TEAM-C/Assets/Scripts/Bomb.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    // Bomb's rigidbody
+    Rigidbody2D rb;
+
+    // Bomber's GameObject
+    private GameObject bomber;
+    // Bomb Health
+    public float maxHealth = 5.0f;
+    // Current Health
+    private float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        health = maxHealth;
+        bomber = GameObject.Find("Bomber");
+        rb.velocity = new Vector2(-bomber.GetComponent<Bomber>().speed, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
