79bfda66ec25176e0f8323f4f708eb32fee7a684
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-12-01 00:28:47 +0000

    Added passing tests

--- RWM-P2-TEAM-C/Assets/Tests/BackgroundLevelTest.cs
+++ RWM-P2-TEAM-C/Assets/Tests/BackgroundLevelTest.cs
@@ -12,7 +12,7 @@ namespace Tests
         private Animator _animator;
         private Runtime2DMovement _2dMovement;
         private PlayerController _playerController;
-
+        private Rigidbody2D _rb;
         [SetUp]
         public void Setup()
         {
@@ -37,24 +37,47 @@ namespace Tests
         public IEnumerator PlayerCanWalkIntoWallTest()
         {
             setUpPlayer();
-            yield return new WaitForSeconds(1.0f);
-            Assert.AreEqual(true, _animator.GetBool("idle"));
+            _2dMovement.handleRightInput();
+            _2dMovement.moveRight();
+            yield return new WaitForSeconds(5.0f);
+            Assert.AreEqual(true, _animator.GetBool("movingRight"));
         }
 
         [UnityTest]
         public IEnumerator PlayerCanWalkOffLedgeTest()
         {
             setUpPlayer();
-            yield return new WaitForSeconds(1.0f);
-            Assert.AreEqual(true, _animator.GetBool("idle"));
+            _rb.position = new Vector2(-53.42f, -4.36f);
+            _2dMovement.handleLeftInput();
+            _2dMovement.moveLeft();
+            yield return new WaitForSeconds(0.6f);
+            Assert.AreEqual(false, _animator.GetBool("grounded"));
         }
 
         [UnityTest]
         public IEnumerator PlayerCanInJumpOntoLedgeTest()
         {
             setUpPlayer();
+            float intialYPos = _rb.position.y;
+            _2dMovement.impluseJumpVel = 30.0f;
+            _2dMovement.handleRightInput();
+            _2dMovement.moveRight();
+            yield return new WaitForSeconds(4.0f);
+            _2dMovement.intialJump();
             yield return new WaitForSeconds(1.0f);
-            Assert.AreEqual(true, _animator.GetBool("idle"));
+            Assert.Greater(_rb.position.y, intialYPos);
+            Assert.AreEqual(true, _animator.GetBool("grounded"));
+            Assert.AreEqual(false, _animator.GetBool("walkingRight"));
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerCannotWalkOffLevelTest()
+        {
+            setUpPlayer();
+            _2dMovement.handleLeftInput();
+            _2dMovement.moveLeft();
+            yield return new WaitForSeconds(1.0f);
+            Assert.AreEqual(false, _animator.GetBool("walkingLeft"));
         }
 
         private void setUpPlayer()
@@ -63,6 +86,7 @@ namespace Tests
             _animator = _player.GetComponent<Animator>();
             _2dMovement = _player.GetComponent<Runtime2DMovement>();
             _playerController = _player.GetComponent<PlayerController>();
+            _rb = _player.GetComponent<Rigidbody2D>();
         }
     }
 }
