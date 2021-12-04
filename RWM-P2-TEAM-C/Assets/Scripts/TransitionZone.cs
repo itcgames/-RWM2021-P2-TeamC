05e60389f90e71179a074ffeeee701e996ba739f
Joshua Dunne <c00241588@itcarlow.ie>
2021-12-04 11:42:51 +0000

    Camera moves to specific Transition Point on Spacebar press

---
+++ RWM-P2-TEAM-C/Assets/Scripts/TransitionZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionZone : MonoBehaviour
+{
+    public int transitionPoint;
+    public ScreenTransition.transitionTypes type;
+
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            Camera.main.GetComponent<CameraMover>().StartMovement(transitionPoint, type);
+        }
+    }
+
+    // when the GameObjects collider arrange for this GameObject to travel to the left of the screen
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Camera.main.GetComponent<CameraMover>().StartMovement(transitionPoint, type);
+        }
+    }
+}
