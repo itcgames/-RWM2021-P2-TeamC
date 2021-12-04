2ee3c26ae9c14cbe600f72623e1df1337e7401cd
Joshua Dunne <c00241588@itcarlow.ie>
2021-12-04 12:47:51 +0000

    Transition Zones can swap between two Transition Points

--- RWM-P2-TEAM-C/Assets/Scripts/TransitionZone.cs
+++ RWM-P2-TEAM-C/Assets/Scripts/TransitionZone.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TransitionZone : MonoBehaviour
 {
-    public int transitionPoint;
+    public int pointOne;
+    public int pointTwo;
     public ScreenTransition.transitionTypes type;
+    bool swap = false;
 
     // begin transition on collision with player only
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player")
         {
-            Camera.main.GetComponent<CameraMover>().StartMovement(transitionPoint, type);
+            if(!Camera.main.GetComponent<CameraMover>().m_moving)
+            {
+                if (!swap)
+                {
+                    Camera.main.GetComponent<CameraMover>().StartMovement(pointOne, type);
+                    swap = true;
+                }
+                else
+                {
+                    Camera.main.GetComponent<CameraMover>().StartMovement(pointTwo, type);
+                    swap = false;
+                }
+            }   
         }
     }
 }
