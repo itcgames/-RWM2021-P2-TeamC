bf6b20c2b09b72aa5a6836a6e98a23ba5e3a9d2b
Joshua Dunne <c00241588@itcarlow.ie>
2021-12-06 17:30:51 +0000

    Door runs function to begin transition on fully open

--- RWM-P2-TEAM-C/Assets/Scripts/TransitionZone.cs
+++ RWM-P2-TEAM-C/Assets/Scripts/TransitionZone.cs
@@ -9,6 +9,23 @@ public class TransitionZone : MonoBehaviour
     public ScreenTransition.transitionTypes type;
     bool swap = false;
 
+    public void BeginTransition()
+    {
+        if (!Camera.main.GetComponent<CameraMover>().m_moving)
+        {
+            if (!swap)
+            {
+                Camera.main.GetComponent<CameraMover>().StartMovement(pointOne, type);
+            }
+            else
+            {
+                Camera.main.GetComponent<CameraMover>().StartMovement(pointTwo, type);
+            }
+
+            swap = !swap;
+        }
+    }
+
     // begin transition on collision with player only
     void OnTriggerEnter2D(Collider2D col)
     {
