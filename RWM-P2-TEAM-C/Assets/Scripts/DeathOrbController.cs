aaf63ae077249f94cb6bb1a8bd3dee34831ea4de
Richard Fleming <c00241212@itcarlow.ie>
2021-12-06 21:14:40 +0000

    ZS-75 shooting sound (#33)
    * Taking doing damage feature Jira Id [ZS-63] (#31)
    
    * Got collisions working with enemy and player
    
    * Added the death aniamtion to megaman
    
    * Fixed issue with death animation
    
    * Added updated p1
    
    * Added in text for players health
    
    * Added Firing Sound Effect
    
    Co-authored-by: Mantas Zalnierius <C00241626@itcarlow.ie>

---
+++ RWM-P2-TEAM-C/Assets/Scripts/DeathOrbController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathOrbController : MonoBehaviour
+{
+    public Vector3 moveDir;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move the death orb
+        transform.position += (moveDir * speed) * Time.deltaTime;
+    }
+}
