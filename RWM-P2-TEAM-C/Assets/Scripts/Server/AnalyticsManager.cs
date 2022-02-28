4a9a32aff3a673381bf7595a9f3637b24572cc3c
Joshua-Dunne <c00241588@itcarlow.ie>
2022-02-28 10:18:27 +0000

    Added server code to send data to server from Unity

---
+++ RWM-P2-TEAM-C/Assets/Scripts/Server/AnalyticsManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyticsManager : MonoBehaviour
+{
+    public static IEnumerator PostMethod(string jsonData)
+    {
+        string url = "http://localhost:5000/upload_data";
+        using (UnityWebRequest request = UnityWebRequest.Put(url, jsonData))
+        {
+            request.method = UnityWebRequest.kHttpVerbPOST;
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Accept", "application/json");
+
+            yield return request.SendWebRequest();
+
+            if (!request.isNetworkError && request.responseCode == (int)HttpStatusCode.OK)
+                Debug.Log("Data successfully sent to the server");
+            else
+                Debug.Log("Error sending data to the server: Error " + request.responseCode);
+        }
+}
