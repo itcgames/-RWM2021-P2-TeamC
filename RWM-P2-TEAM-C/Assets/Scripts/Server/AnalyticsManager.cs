bb9c643bddd70e9e343a7cb500afeb5b58df5e53
Joshua Dunne <c00241588@itcarlow.ie>
2022-03-06 16:49:47 +0000

    ZS-88 Data can now be sent to server.

--- RWM-P2-TEAM-C/Assets/Scripts/Server/AnalyticsManager.cs
+++ RWM-P2-TEAM-C/Assets/Scripts/Server/AnalyticsManager.cs
@@ -40,6 +40,26 @@ public class AnalyticsManager : MonoBehaviour
         data.level = SceneManager.GetActiveScene().buildIndex;
     }
 
+    public IEnumerator PostMethod()
+    {
+        string jsonData = JsonUtility.ToJson(data); // uses pre-made data object
+
+        string url = "http://34.242.150.74/upload_data";
+        using (UnityWebRequest request = UnityWebRequest.Put(url, jsonData))
+        {
+            request.method = UnityWebRequest.kHttpVerbPOST;
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Accept", "application/json");
+
+            yield return request.SendWebRequest();
+
+            if (!request.isNetworkError && request.responseCode == (int)HttpStatusCode.OK)
+                Debug.Log("Data successfully sent to the server");
+            else
+                Debug.Log("Error sending data to the server: Error " + request.responseCode);
+        }
+    }
+
     void Start() { }
 
     void Update() { }
