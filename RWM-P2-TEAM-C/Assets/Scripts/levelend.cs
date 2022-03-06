ce182f915740b65314ac3717762e8e351e008602
emma conlon <emmac2000@outlook.ie>
2022-03-06 21:04:32 +0000

    feature done

---
+++ RWM-P2-TEAM-C/Assets/Scripts/levelend.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class levelend : MonoBehaviour
+{
+   public void restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ RWM-P2-TEAM-C/Assets/Scripts/levelover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class levelover : MonoBehaviour
+{
+   private float timer;
+    private int minutes;
+    private int seconds;
+    private float hours;
+    private bool ended;
+    public Text timing;
+    public GameObject over;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ended = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!ended)
+        {
+            timer += Time.deltaTime;
+            displayText();
+        }
+      
+    }
+    void displayText()
+    {
+        minutes = Mathf.FloorToInt(timer / 60.0f);
+        seconds = Mathf.FloorToInt(timer - minutes * 60.0f);
+        timing.text = "Time Taken: " + minutes + " : " + seconds;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            ended = true;
+            over.SetActive(true);
+        }
+    }
+}
