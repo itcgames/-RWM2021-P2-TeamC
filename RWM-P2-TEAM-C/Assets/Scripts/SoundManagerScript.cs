048b3665b197b8b6b8568428be79d53f0ae605a6
C00241212 <c00241212@itcarlow.ie>
2022-03-07 09:11:08 +0000

    Sound Manager and Basics Added

---
+++ RWM-P2-TEAM-C/Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public static AudioClip temp;
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temp = Resources.Load<AudioClip>("temp");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "temp":
+                audioSrc.PlayOneShot(temp);
+                break;
+        }
+    }
+}
