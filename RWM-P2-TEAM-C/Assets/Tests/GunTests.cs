9e031dc9e139f942fbc4fd3fbe6fc8591f7db33e
Mantas Zalnierius <C00241626@itcarlow.ie>
2022-03-13 19:24:15 +0000

    Task implemented with tests passing (#46)

---
+++ RWM-P2-TEAM-C/Assets/Tests/GunTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class GunTests
+    {
+        private GameObject _player;
+        private Animator _animator;
+        private Runtime2DMovement _2dMovement;
+        private PlayerController _playerController;
+        private GunManager _gunManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            SceneManager.LoadScene("PlayerTestScene", LoadSceneMode.Single);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            SceneManager.UnloadSceneAsync("PlayerTestScene");
+        }
+
+        [UnityTest]
+        public IEnumerator defaultGunTest()
+        {
+            setUpPlayer();
+            yield return new WaitForSeconds(0.5f);
+            Assert.AreEqual(Gun.Normal, _gunManager.getCurrentGun());
+        }
+
+        [UnityTest]
+        public IEnumerator swapGunTest()
+        {
+            setUpPlayer();
+            yield return new WaitForSeconds(0.5f);
+            Assert.AreEqual(Gun.Normal, _gunManager.getCurrentGun());
+            _gunManager.handleSwap();
+            Assert.AreEqual(Gun.SteamPunk, _gunManager.getCurrentGun());
+        }
+
+        [UnityTest]
+        public IEnumerator playerShootingWithDefaultGunTest()
+        {
+            setUpPlayer();
+            yield return new WaitForSeconds(1.0f);
+            _playerController.handleIdlePlayerShooting();
+            yield return new WaitForSeconds(0.1f);
+            GameObject bullet = GameObject.Find("Bullet(Clone)");
+            Bullet bulletScript = bullet.GetComponent<Bullet>();
+            Assert.AreEqual(20.0f, bulletScript.speed); // Speed of the default bullet.
+        }
+
+        [UnityTest]
+        public IEnumerator playerShootingWithSteamPunkGunTest()
+        {
+            setUpPlayer();
+            yield return new WaitForSeconds(1.0f);
+            _gunManager.handleSwap();
+            yield return new WaitForSeconds(0.5f);
+            _playerController.handleIdlePlayerShooting();
+            yield return new WaitForSeconds(0.2f);
+            GameObject bullet = GameObject.Find("Bullet(Clone)");
+            Bullet bulletScript = bullet.GetComponent<Bullet>();
+            Assert.AreEqual(40.0f, bulletScript.speed); // Speed of the steam bullet.
+        }
+
+        private void setUpPlayer()
+        {
+            _player = GameObject.Find("Player");
+            _animator = _player.GetComponent<Animator>();
+            _2dMovement = _player.GetComponent<Runtime2DMovement>();
+            _playerController = _player.GetComponent<PlayerController>();
+            _gunManager = _player.GetComponent<GunManager>();
+        }
+    }
+}
