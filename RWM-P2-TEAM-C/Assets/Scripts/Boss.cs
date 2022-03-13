6bedfb448172f78d0ada747fb961d1ffbb222f48
emma conlon <emmac2000@outlook.ie>
2022-03-13 20:31:41 +0000

    stuff added

---
+++ RWM-P2-TEAM-C/Assets/Scripts/Boss.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public Transform player;
+	public BulletManager _bulletManager;
+	public int direction = -1;
+	public bool isFlipped = false;
+	public bool playerdetected = false;
+	 float _timeBetweenShots = 0.5f;
+
+	private void Update()
+    {
+		LookAtPlayer();
+		
+	}
+    public void LookAtPlayer()
+	{
+		Vector3 temp = transform.localScale;
+		if (temp.x < 0) { direction = 1; }
+		if (temp.x > 0) { direction = -1; }
+
+		Vector3 flipped = transform.localScale;
+		flipped.z *= -1f;
+
+		if (transform.position.x > player.position.x && isFlipped)
+		{
+			transform.localScale = flipped;
+			transform.Rotate(0f, 180f, 0f);
+			isFlipped = false;
+		}
+		else if (transform.position.x < player.position.x && !isFlipped)
+		{
+			transform.localScale = flipped;
+			transform.Rotate(0f, 180f, 0f);
+			isFlipped = true;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.name == "Player")
+		{
+			playerdetected = true;
+			Debug.Log("player detected");
+			shootPlayer();
+			//collision.gameObject.GetComponent<PlayerController>().decreseHealth(10);
+
+
+		}
+
+	}
+
+	void shootPlayer()
+    {
+		if(playerdetected && _bulletManager.canFire())
+		 {
+			Debug.Log("player SHOOT");
+			_bulletManager.shootBullet();
+			StartCoroutine("shootingCooldown");
+		}
+    }
+
+	IEnumerator shootingCooldown()
+	{
+		yield return new WaitForSeconds(_timeBetweenShots);
+		//_isShooting = false;
+		//idleshoot = false;
+		//_2dMovement.setStopMovement(false);
+		yield return new WaitForSeconds(_timeBetweenShots);
+		//_animator.SetBool("isShooting", _isShooting);
+	}
+}
---
+++ RWM-P2-TEAM-C/Assets/Scripts/BossBullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBullet : MonoBehaviour
+{
+    public BulletManager bulletManager;
+    public float bulletDirection;
+    public float speed;
+    public float lifetime;
+
+    // Update is called once per frame
+
+    void Start()
+    {
+        StartCoroutine("livingTime");
+
+        if (null == bulletManager) { bulletManager = GameObject.Find("Boss").GetComponent<BulletManager>(); }
+
+        transform.position = GameObject.Find("Boss").transform.position;
+        bulletDirection = GameObject.Find("Boss").GetComponent<Boss>().direction;
+        bulletManager.increaseBullets();
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3((bulletDirection * speed) * Time.deltaTime, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D t_other)
+    {
+        if (t_other.tag != "Boss")
+        {
+            bulletManager.decreaseBullets(); // decrease total number of bullets
+            StopCoroutine("livingTime"); // stop the co-routine before destroying
+            Destroy(gameObject);
+        }
+    }
+
+
+    IEnumerator livingTime()
+    {
+        yield return new WaitForSeconds(lifetime);
+
+        bulletManager.decreaseBullets(); // decrease total number of bullets
+        Destroy(gameObject); // destroy this after a set amount of time
+
+        yield break;
+    }
+}
