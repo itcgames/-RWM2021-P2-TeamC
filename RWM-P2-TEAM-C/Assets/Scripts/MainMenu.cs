cce69a1f2100359337918ab286e262ddd7b52dc4
emma conlon <emmac2000@outlook.ie>
2022-03-14 16:22:28 +0000

    added animation

---
+++ RWM-P2-TEAM-C/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void train()
+    {
+        Debug.Log("DOESNT EXIST YET");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
