c61ee3571dddb489e20ffb08682c505f9a636843
C00241212 <c00241212@itcarlow.ie>
2022-03-21 09:15:53 +0000

    ZS-127 Bomber and Shrapnel Heights

--- RWM-P2-TEAM-C/Assets/Scripts/Bomb.cs
+++ RWM-P2-TEAM-C/Assets/Scripts/Bomb.cs
@@ -73,7 +73,7 @@ public class Bomb : MonoBehaviour
             gameObject.GetComponent<SpriteRenderer>().sprite = cracked;
             for (int i = 0; i < 3; i++)
             {
-                shrapnel = Instantiate(ShrapnelPassed, new Vector3(rb.position.x, rb.position.y + i, 0), Quaternion.identity);
+                shrapnel = Instantiate(ShrapnelPassed, new Vector3(rb.position.x, (rb.position.y + 1) + i, 0), Quaternion.identity);
                 if(rb.position.x < player.GetComponent<Rigidbody2D>().position.x)
                 {
                     shrapnel.GetComponent<Rigidbody2D>().velocity = new Vector2(this.GetComponentInParent<Bomber>().speed, 0.0f);
