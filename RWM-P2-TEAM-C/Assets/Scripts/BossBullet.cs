4bf027da4e14919325c360eeeb4ea717525a0b75
emma conlon <emmac2000@outlook.ie>
2022-04-03 19:32:20 +0100

    done

--- RWM-P2-TEAM-C/Assets/Scripts/BossBullet.cs
+++ RWM-P2-TEAM-C/Assets/Scripts/BossBullet.cs
@@ -18,8 +18,9 @@ public class BossBullet : MonoBehaviour
         transform.Rotate(0f, 0f, 180f);
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindObjectOfType<PlayerController>();
-        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;//brings it to player
-        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        transform.position = new Vector3(transform.position.x - 1.25f, transform.position.y - 0.8f, transform.position.z);
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        rb.velocity = new Vector2(-transform.position.x, transform.position.y);
        
     }
 
